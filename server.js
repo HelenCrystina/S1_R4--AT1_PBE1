577ebe91e95306c2045b4b6a038fdae9f1e5fed9
HelenCrystina <helenandradee0807@gmail.com>
2025-10-09 08:35:52 -0300

    Organizando código

--- server.js
+++ server.js
@@ -7,27 +7,26 @@ app.use(express.json());
 async function validaLogin(usuario, senha) {
     try {
         if (isNaN(usuario) && !isNaN(senha)) {
-            return (usuario, senha)
-            //console.log("Os valores estão corretos");
+            return (usuario, senha);
         }
     } catch (error) {
         throw new Error(error)
     }
 }
 
-app.post('/login', async (req, res)  => {
+app.post('/login', async (req, res) => {
     try {
-        const { usuario, senha} = await req.body;
-        const erros = validaLogin (usuario, senha);
-        if (usuario=="Admin" && senha==1234) {
-            res.status(201).json({ message: `Os seus dados estão corretos.` })
-        }else{
-            res.status(401).json ({message: `Os dados fornecidos estão incorretos, tente novamente`})
+        const { usuario, senha } = await req.body;
+        const erros = validaLogin(usuario, senha);
+        if (usuario == "Admin" && senha == 1234) {
+            res.status(201).json({ message: `Os seus dados estão corretos.` });
+        } else {
+            res.status(401).json({ message: `Os dados fornecidos estão incorretos, tente novamente` });
         }
-        res.status(error).json({ message: `Dados recebidos com sucesso no servidor.` })
+        res.status(error).json({ message: `Dados recebidos com sucesso no servidor.` });
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: 'Ocorreu um erro ao processar a requisição.', errorMessage: error.message })
+        console.error(error);
+        res.status(500).json({ message: 'Ocorreu um erro ao processar a requisição.', errorMessage: error.message });
     }
 })
 
